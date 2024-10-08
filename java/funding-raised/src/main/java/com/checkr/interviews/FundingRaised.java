9da3875412dd19cbe113b7c824eccebd2836fb1a
Ivaila13 <92357228+Ivaila13@users.noreply.github.com>
2024-10-08 21:33:15 +0300

    method

--- java/funding-raised/src/main/java/com/checkr/interviews/FundingRaised.java
+++ java/funding-raised/src/main/java/com/checkr/interviews/FundingRaised.java
@@ -6,7 +6,7 @@ import java.io.FileReader;
 import java.io.IOException;
 
 public class FundingRaised {
-    public static List<Map<String, String>> where(Map<String, String> options) throws IOException {
+    public static List<Map<String, String>> findByCriteria(Map<String, String> options) throws IOException {
         List<String[]> csvData = new ArrayList<String[]>();
         CSVReader reader = new CSVReader(new FileReader("startup_funding.csv"));
         String[] row = null;
